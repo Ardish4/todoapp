e2a6f866ce04df4e68fa52d5d7b63b6f3b9b4934
Ardish4 <ardishbose@gmail.com>
2025-10-14 14:09:19 +0530

    route, controller and middleware files added

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,4 +9,11 @@ app.get('/', (req, res) => {
   res.send('API is running...')
 })
 
+// import routers
+import userRouter from './routes/user.route.js'
+import todoRouter from './routes/todo.route.js'
+
+app.use('/api/users', userRouter)
+app.use('/api/todos', todoRouter)
+
 export default app
